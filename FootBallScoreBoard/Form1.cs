de9673300049487ab00479e6b5b4fd3730cb60d6
cuviet <vietwiin@gmail.com>
2022-06-30 16:58:05 -0500

    Cleaned up Form1 code by putting the code that loads the scoreboard into methods of the ScoreBoard Data class

--- FootBallScoreBoard/Form1.cs
+++ FootBallScoreBoard/Form1.cs
@@ -13,15 +13,10 @@ namespace FootBallScoreBoard
         {
             InitializeComponent();
 
-            this.homeTeamScore.Text = ScoreBoardData.ScoreHomeTeam;
-            this.visitorTeamScore.Text = ScoreBoardData.ScoreVisitorTeam;
-            this.homeTimeOutNum.Text = ScoreBoardData.TimeOutLeftHomeTeam;
-            this.visitorTimeOutNum.Text = ScoreBoardData.TimeOutLeftVisitorTeam;
-            this.numBallPosition.Text = ScoreBoardData.PositionOfBallOnField;
-            this.numQuarter.Text = ScoreBoardData.CurrentQuarterOfGame;
-            this.numDown.Text = ScoreBoardData.CurrentNumberOfDown;
-            this.numYardsToGo.Text = ScoreBoardData.NumberOfYardsForFirstDown;
-            this.timerLabel.Text = ScoreBoardData.Minutes + ":" + ScoreBoardData.Seconds;
+            // call to method to load data into the scoreboard
+            ScoreBoardData.loadDataToScoreBoard(this);
+
+            ScoreBoardData.loadTimeToScoreBoard(this);
 
         }
 
@@ -29,44 +24,15 @@ namespace FootBallScoreBoard
         {
             this.button2.Enabled = false;
         }
-        public string appendZero(double str)
-        {
-            if (str <= 9)
-                return "0" + str;
-            else
-                return str.ToString();
-        }
 
         private void updateControllerButton_Click(object sender, EventArgs e)
         {
 
             ScoreBoardUpdateController = new Form2();
 
-            ScoreBoardUpdateController.homeTeamUpdateInput.Text = ScoreBoardData.ScoreHomeTeam;
-            ScoreBoardUpdateController.visitorTeamUpdateInput.Text = ScoreBoardData.ScoreVisitorTeam;
-            ScoreBoardUpdateController.homeTeamUpdateTolInput.Text = ScoreBoardData.TimeOutLeftHomeTeam;
-            ScoreBoardUpdateController.visitorTeamUpdateTolInput.Text = ScoreBoardData.TimeOutLeftVisitorTeam;
-            ScoreBoardUpdateController.ballPositionUpdateInput.Text = ScoreBoardData.PositionOfBallOnField;
-            ScoreBoardUpdateController.qtrUpdateInput.Text = ScoreBoardData.CurrentQuarterOfGame;
-            ScoreBoardUpdateController.downUpdateInput.Text = ScoreBoardData.CurrentNumberOfDown;
-            ScoreBoardUpdateController.toGoUpdateInput.Text = ScoreBoardData.NumberOfYardsForFirstDown;
+            ScoreBoardData.loadDataToScoreBoard(ScoreBoardUpdateController);
 
-            string[] timerNumbers = this.timerLabel.Text.Split(':');
-
-            ScoreBoardData.Minutes = timerNumbers[0];
-            ScoreBoardData.Seconds = timerNumbers[1];
-
-            for (int i = 0; i < 60; i++)
-            {
-                ScoreBoardUpdateController.comboBox2.Items.Add(i.ToString());
-            }
-            ScoreBoardUpdateController.comboBox2.SelectedIndex = int.Parse(timerNumbers[1]);
-
-            for (int i = 0; i <= 15; i++)
-            {
-                ScoreBoardUpdateController.comboBox1.Items.Add(i.ToString());
-            }
-            ScoreBoardUpdateController.comboBox1.SelectedIndex = int.Parse(timerNumbers[0]);
+            ScoreBoardData.loadTimeToScoreBoard(this, ScoreBoardUpdateController);
 
             ScoreBoardUpdateController.Show();
             this.Hide();
@@ -88,8 +54,9 @@ namespace FootBallScoreBoard
                 int seconds = totalSeconds - (minutes * 60);
 
 
-                newMinute = this.appendZero(minutes);
-                newSecond = this.appendZero(seconds);
+                newMinute = ScoreBoardData.appendZero(minutes);
+                newSecond = ScoreBoardData.appendZero(seconds);
+
                 this.timerLabel.Text = newMinute + ":" + newSecond;
             }
 
--- FootBallScoreBoard/ScoreBoardData.cs
+++ FootBallScoreBoard/ScoreBoardData.cs
@@ -20,5 +20,87 @@ namespace FootBallScoreBoard
         public static string Minutes { get; set; } = "15";
         public static string Seconds { get; set; } = "00";
 
+        /// <summary>
+        /// Loads the data from the ScoreboardData class into the controller for the scoreboard
+        /// except for the minutes and seconds
+        /// </summary>
+        /// <param name="controllerBoard">This is the controller for the scoreboard</param>
+        public static void loadDataToScoreBoard(Form2 controllerBoard)
+        {
+            controllerBoard.homeTeamUpdateInput.Text = ScoreBoardData.ScoreHomeTeam;
+            controllerBoard.visitorTeamUpdateInput.Text = ScoreBoardData.ScoreVisitorTeam;
+            controllerBoard.homeTeamUpdateTolInput.Text = ScoreBoardData.TimeOutLeftHomeTeam;
+            controllerBoard.visitorTeamUpdateTolInput.Text = ScoreBoardData.TimeOutLeftVisitorTeam;
+            controllerBoard.ballPositionUpdateInput.Text = ScoreBoardData.PositionOfBallOnField;
+            controllerBoard.qtrUpdateInput.Text = ScoreBoardData.CurrentQuarterOfGame;
+            controllerBoard.downUpdateInput.Text = ScoreBoardData.CurrentNumberOfDown;
+            controllerBoard.toGoUpdateInput.Text = ScoreBoardData.NumberOfYardsForFirstDown;
+        }
+
+        /// <summary>
+        /// Overloaded function that will load the data from the ScoreBoardData class into the
+        /// initial scoreboard except for minutes and seconds
+        /// </summary>
+        /// <param name="OGScoreBoard">This is the original scoreboard</param>
+        public static void loadDataToScoreBoard(Form1 OGScoreBoard)
+        {
+            OGScoreBoard.homeTeamScore.Text = ScoreBoardData.ScoreHomeTeam;
+            OGScoreBoard.visitorTeamScore.Text = ScoreBoardData.ScoreVisitorTeam;
+            OGScoreBoard.homeTimeOutNum.Text = ScoreBoardData.TimeOutLeftHomeTeam;
+            OGScoreBoard.visitorTimeOutNum.Text = ScoreBoardData.TimeOutLeftVisitorTeam;
+            OGScoreBoard.numBallPosition.Text = ScoreBoardData.PositionOfBallOnField;
+            OGScoreBoard.numQuarter.Text = ScoreBoardData.CurrentQuarterOfGame;
+            OGScoreBoard.numDown.Text = ScoreBoardData.CurrentNumberOfDown;
+            OGScoreBoard.numYardsToGo.Text = ScoreBoardData.NumberOfYardsForFirstDown;
+        }
+
+        /// <summary>
+        /// Loads the time to the scoreboard by copying the minutes and seconds into the label
+        /// </summary>
+        /// <param name="OGScoreBoard">This is the original Scoreboard</param>
+        public static void loadTimeToScoreBoard(Form1 OGScoreBoard)
+        {
+            OGScoreBoard.timerLabel.Text = ScoreBoardData.Minutes + ":" + ScoreBoardData.Seconds;
+        }
+
+        /// <summary>
+        /// Overloaded function to load the time into the scoreboard controller by splitting up the minutes
+        /// and seconds from the timer label and storing them in an array that is passed to each member of 
+        /// the ScoreBoardData Class
+        /// </summary>
+        /// <param name="Controller">The update controller Form</param>
+        public static void loadTimeToScoreBoard(Form1 OGScoreBoard, Form2 Controller)
+        {
+            string[] timerNumbers = OGScoreBoard.timerLabel.Text.Split(':');
+
+            ScoreBoardData.Minutes = timerNumbers[0];
+            ScoreBoardData.Seconds = timerNumbers[1];
+
+            for (int i = 0; i < 60; i++)
+            {
+                Controller.comboBox2.Items.Add(i.ToString());
+            }
+            Controller.comboBox2.SelectedIndex = int.Parse(timerNumbers[1]);
+
+            for (int i = 0; i <= 15; i++)
+            {
+                Controller.comboBox1.Items.Add(i.ToString());
+            }
+            Controller.comboBox1.SelectedIndex = int.Parse(timerNumbers[0]);
+        }
+
+        /// <summary>
+        /// The function adds a zero before the number if it is only  1 digit
+        /// </summary>
+        /// <param name="str">The number that is in the clock label</param>
+        /// <returns>The number with a zero preceding it</returns>
+        public static string appendZero(double str)
+        {
+            if (str <= 9)
+                return "0" + str;
+            else
+                return str.ToString();
+        }
+
     }
 }
